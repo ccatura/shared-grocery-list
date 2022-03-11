d705a45171e5d3a196c2bf7a6124942d1cff4fce
ccatura <charlie@charliecatura.com>
2022-03-11 11:49:31 -0500

    added button to prepend text when chatting

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var x;
+var submitChat = document.querySelector('.submit-chat');
+
+submitChat.onclick = function() {
+    var newChat = document.createElement("div"); 
+    newChat.className = "chat-line";
+    var chatBox = document.querySelector('.chat-box');
+        newChat.textContent = Math.random(5);
+    chatBox.prepend(newChat);
+}
