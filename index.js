16b32106aea2abcdf8f51bd8089fabda7f54fd85
ccatura <charlie@charliecatura.com>
2022-03-11 12:33:15 -0500

    added more chat functionality

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var submitChat = document.querySelector('.submit-chat');
 
+
 submitChat.onclick = function() {
+    var chatToSubmit = document.getElementById('chat-to-submit');
     var newChat = document.createElement("div"); 
-    newChat.className = "chat-line";
-    var chatBox = document.querySelector('.chat-box');
-        newChat.textContent = Math.random(5);
-    chatBox.prepend(newChat);
+    var chat = chatToSubmit.value;
+    if (chat) {
+        newChat.className = "chat-line";
+        var chatBox = document.querySelector('.chat-box');
+            newChat.textContent = chat;
+        chatBox.prepend(newChat);
+        chatToSubmit.value = "";
+    }
 }
