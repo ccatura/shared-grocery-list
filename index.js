208c22e585528a777230cd5a669aac7e003661fa
ccatura <charlie@charliecatura.com>
2022-09-05 13:11:31 -0400

    made major changes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
+// CHAT
 var submitChat = document.querySelector('.submit-chat');
 var chatToSubmit = document.getElementById('chat-to-submit');
-
-
-
 
 function updateChat() {
     var newChat = document.createElement("div"); 
@@ -19,10 +17,23 @@ function updateChat() {
     }
 }
 
-
 submitChat.addEventListener('click', updateChat);
 chatToSubmit.addEventListener('keyup', function(event) {
     if (event.keyCode === 13) {
         updateChat();
     }
+});
+
+
+// ADD ITEM BUTTON
+var addItemButton = document.querySelector('#add-item-button');
+var addItemInputbox = document.querySelector('.add-item-input-box');
+var addItemCloseButton = document.querySelector('#add-item-close-button');
+
+addItemButton.addEventListener('click', function() {
+    addItemInputbox.style.display = "flex";
+});
+
+addItemCloseButton.addEventListener('click', function() {
+    addItemInputbox.style.display = "none";
 });
